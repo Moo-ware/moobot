363e71d193d9f07565a53b3ba355565cd59e3dce
Mal <56319809+Moo-ware@users.noreply.github.com>
2023-02-24 20:46:25 -0800

    v0.02
    -Added Boss Timer
    -Added EU Market

--- functions.py
+++ functions.py
@@ -1,6 +1,8 @@
 import datetime
 import json
 import requests
+from pytz import timezone
+
 
 eLevel = {
   0  : "",
@@ -12,8 +14,127 @@ eLevel = {
   20 : "PEN",
 }
 
+spawnTimes = {
+    'Monday' : {
+        '0015' : 'Kutum & Karanda',
+        '0200' : 'Karanda',
+        '0500' : 'Kzarka',
+        '0900' : 'Kzarka',
+        '1200' : 'Offin	',
+        '1600' : 'Kutum	',
+        '1900' : 'Nouver',
+        '2215' : 'Kzarka'
+    },
+    'Tuesday' : {
+        '0015' : 'Karanda',
+        '0200' : 'Kutum',
+        '0500' : 'Kzarka',
+        '0900' : 'Nouver',
+        '1200' : 'Kutum',
+        '1600' : 'Nouver',
+        '1900' : 'Karanda',
+        '2215' : 'Garmoth'
+    },
+    'Wednesday' : {
+        '0015' : 'Kutum & Kzarka',
+        '0200' : 'Karanda',
+        '0900' : 'Karanda',
+        '1200' : 'Nouver',
+        '1600' : 'Kutum & Offin',
+        '1900' : 'Vell',
+        '2215' : 'Karanda & Kzarka',
+        '2315' : 'Quint & Muraka'
+    },
+    'Thursday' : {
+        '0015' : 'Nouver',
+        '0200' : 'Kutum',
+        '0500' : 'Kzarka',
+        '0900' : 'Kutum',
+        '1200' : 'Nouver',
+        '1600' : 'Kzarka',
+        '1900' : 'Kutum',
+        '2215' : 'Garmoth'
+    },
+    'Friday' : {
+        '0015' : 'Kzarka & Karanda',
+        '0200' : 'Nouver',
+        '0500' : 'Karanda',
+        '0900' : 'Kutum',
+        '1200' : 'Karanda',
+        '1600' : 'Nouver',
+        '1900' : 'Kzarka',
+        '2215' : 'Kutum & Kzarka'
+    },
+    'Saturday' : {
+        '0015' : 'Karanda',
+        '0200' : 'Offin',
+        '0500' : 'Nouver',
+        '0900' : 'Kutum',
+        '1200' : 'Nouver',
+        '1600' : 'Quint & Muraka',
+        '1900' : 'Kzarka & Karanda'
+    },
+    'Sunday' : {
+        '0015' : 'Nouver & Kutum',
+        '0200' : 'Kzarka',
+        '0500' : 'Kutum',
+        '0900' : 'Nouver',
+        '1200' : 'Kzarka',
+        '1600' : 'Vell',
+        '1900' : 'Garmoth',
+        '2215' : 'Kzarka & Nouver'
+    },
+}
+
+dayOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
+
+def spawnFromNow():
+    spawnList = []
+    eastern = timezone('US/Eastern')
+    day = datetime.datetime.now(eastern).strftime('%H%M-%A').split('-')
+    for i in spawnTimes[day[1]]:
+        if int(noOctal(i)) > int(noOctal(day[0])):
+            time = [int(noOctal(i[0:2])) - int(noOctal(day[0][0:2])), int(noOctal(i[2:])) - int(noOctal(day[0][2:]))]
+            timefromnow = str(datetime.timedelta(minutes=(time[0] * 60 + time[1] - 1)))[:-3]
+            spawnList.append([timefromnow, spawnTimes[day[1]][i]])
+        
+        elif int(noOctal(i)) == int(noOctal(day[0])) or (int(noOctal(day[0])) - int(noOctal(i))) <= 4 :
+            spawnList.append(['SPAWNED', spawnTimes[day[1]][i]])
+    
+    if len(spawnList) < 3:
+        nextDay = day
+        if nextDay[1] == 'Sunday':
+            nextDay[1] = 'Monday'
+        else:
+            nextDay[1] = dayOfWeek[dayOfWeek.index(day[1]) + 1]
+        for i in spawnTimes[nextDay[1]]:
+            time = [int(noOctal(i[0:2])) - int(noOctal(nextDay[0][0:2])), int(noOctal(i[2:])) - int(noOctal(nextDay[0][2:]))]
+            timefromnow = str(datetime.timedelta(minutes=(time[0] * 60 + time[1] - 1)))[:-3]
+            spawnList.append([timefromnow, spawnTimes[nextDay[1]][i], 1])
+        return spawnList
+    else:
+        return spawnList
+  
+def specialSpawns():
+    specialSpawnList = []
+
+
 def GetWaitlist():
     url = 'https://na-trade.naeu.playblackdesert.com/Home/GetWorldMarketWaitList'
+    headers = {
+    "Content-Type": "application/json",
+    "User-Agent": "BlackDesert"
+    }
+    payload = {}
+
+    response = requests.request('POST', url, json=payload, headers=headers)
+    
+    x = json.loads(response.text)
+    return x
+
+def GetWaitlistEU():
+    url = 'https://eu-trade.naeu.playblackdesert.com/Home/GetWorldMarketWaitList'
     headers = {
     "Content-Type": "application/json",
     "User-Agent": "BlackDesert"
@@ -32,3 +153,10 @@ def matchEnhancement(key):
 def timeCalc(timestamp):
     td = datetime.datetime.fromtimestamp(timestamp / 1e3) - datetime.datetime.now()
     return int(round(td.total_seconds() / 60))
+
+def noOctal(num):
+    for x, i in enumerate(num):
+        if int(i) == 0:
+            continue  
+        return (num[x:])
+    return 0
--- main.py
+++ main.py
@@ -18,9 +18,11 @@ for bot_command in os.listdir('cogs'):
         name = "cogs." + bot_command[:-3]
         client.load_extension(name)
 
+
 @client.command(aliases = ['p'])
 async def ping(ctx):
     await ctx.send('Pong! That took **{}** ms.'.format(round(client.latency, 5) * 1000))
+
 
 @client.command()
 async def helptest(ctx):
@@ -31,20 +33,74 @@ async def helptest(ctx):
     embed.set_footer(text="Prefix:  [ . ] ")
     await ctx.send(embed=embed)
 
+
 @client.command(aliases = ['q'])
-async def queue(ctx):
-    waitList = GetWaitlist()
-    embed=discord.Embed(title="In Registrations Queue:", url="https://na-trade.naeu.playblackdesert.com/Home/list/wait", color=0xfe9a9a)
-    embed.set_author(name="Central Market")
-    if len(waitList["_waitList"]) != 0:
-        for i in waitList["_waitList"]:
-            embed.add_field(name="{}: {}".format(matchEnhancement(i['chooseKey']), i['name']), 
-            value="{:,}  ```ansi\n \u001b[0;47m\u001b[1;35m    In {} min    \u001b[0m```".format(i['_pricePerOne'], timeCalc(i['_waitEndTime'])), inline=True)
+async def queue(ctx, region='na'):
+    if region.lower() == 'eu':        
+        waitList = GetWaitlistEU()
+        embed=discord.Embed(title="In Registrations Queue:", url="https://eu-trade.naeu.playblackdesert.com/Home/list/wait", color=0xfe9a9a)
+        embed.set_author(name="Central Market [EU]")
+        if len(waitList["_waitList"]) != 0:
+            for i in waitList["_waitList"]:
+                embed.add_field(name="{}: {}".format(matchEnhancement(i['chooseKey']), i['name']), 
+                value="{:,}  ```ansi\n \u001b[0;47m\u001b[1;35m    In {} min    \u001b[0m```".format(i['_pricePerOne'], timeCalc(i['_waitEndTime'])), inline=True)
+        else:
+            embed.add_field(name="", value="```ansi\n \u001b[0;47m\u001b[1;35m    Nothing is in Queue    \u001b[0m```", inline=False)
+        
+        embed.set_footer(text=datetime.datetime.now())
+        await ctx.send(embed=embed)
     else:
-        embed.add_field(name="", value="```ansi\n \u001b[0;47m\u001b[1;35m    Nothing is in Queue    \u001b[0m```", inline=False)
+        waitList = GetWaitlist()
+        embed=discord.Embed(title="In Registrations Queue:", url="https://na-trade.naeu.playblackdesert.com/Home/list/wait", color=0xfe9a9a)
+        embed.set_author(name="Central Market [NA]")
+        if len(waitList["_waitList"]) != 0:
+            for i in waitList["_waitList"]:
+                embed.add_field(name="{}: {}".format(matchEnhancement(i['chooseKey']), i['name']), 
+                value="{:,}  ```ansi\n \u001b[0;47m\u001b[1;35m    In {} min    \u001b[0m```".format(i['_pricePerOne'], timeCalc(i['_waitEndTime'])), inline=True)
+        else:
+            embed.add_field(name="", value="```ansi\n \u001b[0;47m\u001b[1;35m    Nothing is in Queue    \u001b[0m```", inline=False)
+        
+        embed.set_footer(text=datetime.datetime.now())
+        await ctx.send(embed=embed)
+
+
+@client.command()
+async def boss(ctx):
+    spawn = spawnFromNow()
+    del spawn[4:]
+
+    description = ''
+    for i in spawn:
+        if len(i) == 2:
+            if i[0] == 'SPAWNED':
+                newstring = '{0:25}{1}\n'.format(i[1], i[0])
+                description = description + newstring
     
-    embed.set_footer(text=datetime.datetime.now())
+            elif int(i[0][:-3]) == 1:
+                timestr = 'In {} hour and {} minutes'.format(i[0][:-3], i[0][-2:])
+                newstring = '{0:25}{1}\n'.format(i[1], timestr)
+                description = description + newstring
+            else:
+                timestr = 'In {} hours and {} minutes'.format(i[0][:-3], i[0][-2:])
+                newstring = '{0:25}{1}\n'.format(i[1], timestr)
+                description = description + newstring
+            
+        elif len(i) == 3:
+            if int(i[0][8:-3]) == 1:
+                timestr = 'In {} hour and {} minutes'.format(i[0][8:-3], i[0][-2:])
+                newstring = '{0:25}{1}\n'.format(i[1], timestr)
+                description = description + newstring
+            else:
+                timestr = 'In {} hours and {} minutes'.format(i[0][8:-3], i[0][-2:])
+                newstring = '{0:25}{1}\n'.format(i[1], timestr)
+                description = description + newstring
+            
+        else:
+            print('Error')
+    
+    embed=discord.Embed(title="BDO Boss Timer:", description='```diff\n-{}\n```'.format(description), color=0xfe9a9a)
     await ctx.send(embed=embed)
+
 
 # Gets bot token from apikey.json file
 f = open('apikey.json')
